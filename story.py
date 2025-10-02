5c5c6c8b7e121575dc8f75b18b09e4b63b5b8aba
822012MN <822012@manadulted.ac.uk>
2025-10-02 13:10:20 +0000

    added colour choice to story

--- story.py
+++ story.py
@@ -1,3 +1,5 @@
 
 name = input("What is your name? ")
 print(f"Welcome {name}, let's start a story together!")
+colour = input("What is your favourite colour?")
+print(f"Great choice, {colour} is a wonderful colour!")
